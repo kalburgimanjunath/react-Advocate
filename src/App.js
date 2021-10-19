7e8b48ef6630013a8e3b8620e4181a3c06ed134b
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-19 15:39:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+import { Header, Footer } from './components/index';
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
