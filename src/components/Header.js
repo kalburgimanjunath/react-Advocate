aebdf730b77c8c7905ef13edc9466a2dbe87d5f5
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-19 15:42:59 +0530

    3 files modified, 3 added, 3 deleted

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+export default function Header() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/laywers">Laywers</Link>
+        </li>
+        <li>
+          <Link to="/login">Start here</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
